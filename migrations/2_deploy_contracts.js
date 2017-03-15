8bed81edd078e37b08fe798daad0ba708082a812
Damien Conroy <damien.conroy@gmail.com>
2017-03-15 00:45:30 +0000

    added code to create a project when the funding hub is deployed

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,8 +6,18 @@ var IProject = artifacts.require("./IProject.sol");
 module.exports = function(deployer) {
   deployer.deploy(ProjectSetManager);
   deployer.link(ProjectSetManager, FundingHub);
-  deployer.deploy(FundingHub);
+  deployer.deploy(FundingHub)
   deployer.deploy(IProject);
   deployer.deploy(Project);
-  //TODO: add call to createProject
+  deployer.new(FundingHub);
+
+  deployer.then(function(){
+    return FundingHub.deployed()
+    .then(function(_fundingHub) {
+      var target = 3001;
+      var deadline = 1513115725; // later in 2017
+      console.log(deployer);
+      return _fundingHub.createProject('0x0', target, deadline);
+    });
+  });
 };
